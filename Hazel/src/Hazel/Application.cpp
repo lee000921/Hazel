1c30b99d66b373b47d7f2a53312510a6952272b2
Lee <jlucslx@163.com>
2024-10-21 17:26:58 +0800

    add event system

--- Hazel/src/Hazel/Application.cpp
+++ Hazel/src/Hazel/Application.cpp
@@ -1,5 +1,8 @@
 #include "Application.h"
 
+#include "Hazel/Events/ApplicationEvent.h"
+#include "Hazel/Log.h"
+
 namespace Hazel {
 
 	Application::Application() {
@@ -11,7 +14,14 @@ namespace Hazel {
 	}
 
 	void Application::Run() {
-
+		WindowResizeEvent e(1280, 720);
+		if (e.IsInCategory(EventCategoryApplication)) {
+			// TODO: Can not directly use HZ_TRACE(e) and don't find the reason yet.
+			HZ_TRACE(e.ToString());
+		}
+		if (e.IsInCategory(EventCategoryInput)) {
+			HZ_TRACE(e.ToString());
+		}
 		while (true);
 	}
 }
