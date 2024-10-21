c3dad36032c3f055ba4fff42a9670b2e8e7408e7
Lee <jlucslx@163.com>
2024-10-21 23:17:15 +0800

    Added Window Class and GLFW

--- Hazel/src/Hazel/Application.cpp
+++ Hazel/src/Hazel/Application.cpp
@@ -2,12 +2,13 @@
 #include "Application.h"
 
 #include "Hazel/Events/ApplicationEvent.h"
-#include "Hazel/Log.h"
+
+#include <GLFW/glfw3.h>
 
 namespace Hazel {
 
 	Application::Application() {
-
+		m_Window = std::unique_ptr<Window>(Window::Create());
 	}
 
 	Application::~Application() {
@@ -15,14 +16,11 @@ namespace Hazel {
 	}
 
 	void Application::Run() {
-		WindowResizeEvent e(1280, 720);
-		if (e.IsInCategory(EventCategoryApplication)) {
-			// TODO: Can not directly use HZ_TRACE(e) and don't find the reason yet.
-			HZ_TRACE(e.ToString());
+		while (m_Running) {
+
+			glClearColor(1, 0, 1, 1);
+			glClear(GL_COLOR_BUFFER_BIT);
+			m_Window->OnUpdate();
 		}
-		if (e.IsInCategory(EventCategoryInput)) {
-			HZ_TRACE(e.ToString());
-		}
-		while (true);
 	}
 }
