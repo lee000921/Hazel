2f4fbfaa7d04ff2115cc49850a006032c347efae
Lee <jlucslx@163.com>
2024-10-30 20:04:07 +0800

    S016 ImGul事件

--- Hazel/src/Hazel/Events/MouseEvent.h
+++ Hazel/src/Hazel/Events/MouseEvent.h
@@ -44,7 +44,7 @@ namespace Hazel {
 
 	class HAZEL_API MouseButtonEvent : public Event {
 	public:
-		inline int CetMouseButton() const { return m_Button; }
+		inline int GetMouseButton() const { return m_Button; }
 
 		EVENT_CLASS_CATEGORY(EventCategoryMouse | EventCategoryInput)
 	protected:
