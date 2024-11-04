4eca055b5a1b1747260ca80aa8c720986ae07b24
Lee <jlucslx@163.com>
2024-11-04 22:57:06 +0800

    S035 创建正交相机

--- Hazel/src/Hazel/Application.cpp
+++ Hazel/src/Hazel/Application.cpp
@@ -13,7 +13,9 @@ namespace Hazel {
 
 	Application* Application::s_Instance = nullptr;
 
-	Application::Application() {
+	Application::Application() 
+		: m_Camera(-1.6f, 1.6f, -0.9f, 0.9f) {
+		
 		HZ_CORE_ASSERT(!s_Instance, "Application already exists!");
 		s_Instance = this;
 
@@ -77,15 +79,17 @@ namespace Hazel {
 			
 			layout(location = 0) in vec3 a_Position;	
 			layout(location = 1) in vec4 a_Color;	
+			
+			uniform mat4 u_ViewProjection;
 
 			out vec3 v_Position;		
 			out vec4 v_Color;		
 
-			void main()
+			void main() 
 			{	
 				v_Position = a_Position;
 				v_Color = a_Color;
-				gl_Position = vec4(a_Position, 1.0);
+				gl_Position = u_ViewProjection * vec4(a_Position, 1.0);
 			}
 		)";
 
@@ -108,11 +112,13 @@ namespace Hazel {
 		std::string blueShaderVertexSrc = R"(
 			#version 330 core
 			
-			layout(location = 0) in vec3 a_Position;				
+			layout(location = 0) in vec3 a_Position;	
+
+			uniform mat4 u_ViewProjection;			
 
 			void main()
 			{	
-				gl_Position = vec4(a_Position, 1.0);
+				gl_Position = u_ViewProjection * vec4(a_Position, 1.0);
 			}
 		)";
 
@@ -163,13 +169,13 @@ namespace Hazel {
 			RenderCommand::SetClearColor({ 0.1f, 0.1f, 0.1f, 1 });
 			RenderCommand::Clear();
 
-			Renderer::BeginScene();
+			m_Camera.SetPosition({0.5f, 0.5f, 0});
+			m_Camera.SetRotation(45.0f);
 
-			m_BlueShader->Bind();
-			Renderer::Submit(m_SquareVA);
+			Renderer::BeginScene(m_Camera);
 
-			m_Shader->Bind();
-			Renderer::Submit(m_VertexArray);
+			Renderer::Submit(m_BlueShader, m_SquareVA);
+			Renderer::Submit(m_Shader, m_VertexArray);
 
 			Renderer::EndScene();
 
