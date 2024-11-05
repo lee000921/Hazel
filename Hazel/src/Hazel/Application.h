62bc7cb2fc66a197c6a81d37613784423cc5e445
Lee <jlucslx@163.com>
2024-11-05 14:39:04 +0800

    S037 Timestep和增量时间

--- Hazel/src/Hazel/Application.h
+++ Hazel/src/Hazel/Application.h
@@ -7,6 +7,8 @@
 #include "Hazel/Events/Event.h"
 #include "Hazel/Events/ApplicationEvent.h"
 
+#include "Hazel/Core/Timestep.h"
+
 #include "Hazel/ImGui/ImGuiLayer.h"
 
 namespace Hazel {
@@ -28,10 +30,13 @@ namespace Hazel {
 	private:
 		bool OnWindowClose(WindowCloseEvent& e);
 
+	private:
 		std::unique_ptr<Window> m_Window;
 		ImGuiLayer* m_ImGuiLayer;
 		bool m_Running = true;
 		LayerStack m_LayerStack;
+		float m_LastFrameTime = 0.0f;
+
 	private:
 		static Application* s_Instance;
 	};
