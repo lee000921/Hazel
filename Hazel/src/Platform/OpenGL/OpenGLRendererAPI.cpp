dd7f34e4e832f9ef56793b6d162881758c47f601
Lee <jlucslx@163.com>
2024-11-07 20:01:22 +0800

    real blend

--- Hazel/src/Platform/OpenGL/OpenGLRendererAPI.cpp
+++ Hazel/src/Platform/OpenGL/OpenGLRendererAPI.cpp
@@ -4,17 +4,23 @@
 #include <glad/glad.h>
 
 namespace Hazel {
+
+	void OpenGLRendererAPI::Init() {
+		glEnable(GL_BLEND);
+		glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+	}
 	
-	void Hazel::OpenGLRendererAPI::SetClearColor(const glm::vec4& color) {
+	void OpenGLRendererAPI::SetClearColor(const glm::vec4& color) {
 	
 		glClearColor(color.r, color.g, color.b, color.a);
 	}
 
-	void Hazel::OpenGLRendererAPI::Clear() {
+	void OpenGLRendererAPI::Clear() {
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	}
 
-	void Hazel::OpenGLRendererAPI::DrawIndexed(const Ref<VertexArray>& VertexArray) {
+	void OpenGLRendererAPI::DrawIndexed(const Ref<VertexArray>& VertexArray) {
 		glDrawElements(GL_TRIANGLES, VertexArray->GetIndexBuffer()->GetCount(), GL_UNSIGNED_INT, nullptr);
 	}
 }
